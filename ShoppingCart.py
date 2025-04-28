0433271c51f4bdf44132e31b6c726d25ce294f48
carlosgamer101 <atogocarlos9@gmail.com>
2025-04-28 09:19:05 +0300

    fAssignment

--- ShoppingCart.py
+++ ShoppingCart.py
@@ -4,25 +4,21 @@ class ShoppingCart:
         self.items = []
 
     def add_item(self, item_name: str, qty: int, unit_price: float):
-
         self.items.append((item_name, qty, unit_price))
 
     def remove_item(self, item_name: str):
-
         for item in self.items:
             if item[0] == item_name:
                 self.items.remove(item)
                 break
 
     def calculate_total(self) -> float:
-
         total = 0.0
         for name, qty, price in self.items:
             total += qty * price
         return total
 
     def cart_contents(self):
-
         print("Cart Contents: ")
         for name, qty, price in self.items:
             print(f"  {name}:- {qty} @ Ksh {price:.2f} each")
